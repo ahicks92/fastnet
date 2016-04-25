e4ea26264c3a7119eb1cbb5049109ae9b6af82ef
Austin Hicks <camlorn@camlorn.net>
2016-04-24 20:26:42 -0400

    Handlers must be send.

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ Fastnet does not distinguish between clients and servers.  This is used both for
 pub struct Server;
 
 impl Server {
-    pub fn new<H: Handler>(addr: net::SocketAddr, handler: H)->Result<Server> {
+    pub fn new<H: Handler+Send>(addr: net::SocketAddr, handler: H)->Result<Server> {
         Ok(Server::default())
     }
 }
