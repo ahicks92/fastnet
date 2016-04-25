7fb3ff27aec1e5e99aaf16bbda55910a5c6ac3fc
Austin Hicks <camlorn@camlorn.net>
2016-04-24 20:03:25 -0400

    Start mocking up the public API.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+/*! The main Fastnet module.
+
+This is the low-level API.  If your goal is extremely high-performance usage, this is the API you want.  See the blocking module for a simpler API which is less annoying for common use cases.*/
 #![allow(warnings)]
 
 extern crate byteorder;
@@ -8,3 +11,40 @@ extern crate crc;
 mod packets;
 mod server;
 mod status_translator;
+
+use std::{result, io, net};
+
+///Represents a Fastnet error.
+#[derive(Debug, Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
+enum Error {
+    TimedOut,
+    HostNotFound,
+    PeerNotFound,
+    MessageTooLarge,
+}
+
+type Result<T> = result::Result<T, Error>;
+
+/**A Fastnet server.
+
+Fastnet does not distinguish between clients and servers.  This is used both for connecting to other peers and listening for incoming connections.*/
+struct Server<H: Handler> {
+    handler: H,
+}
+
+impl<H: Handler> Server<H> {
+    pub fn new(addr: net::SocketAddr, handler: H)->Result<Server<H>> {
+        Ok(Server {
+            handler: handler,
+        })
+    }
+}
+
+/**An event handler.
+
+The methods in this trait are called in a thread which is running in the background, not on the main thread.  None of them should ever block.*/
+pub trait Handler {
+    fn connected(&mut self, id: u64, request_id: Option<u64>);
+    fn disconnected(&mut self, id: u64, request_id: Option<u64>);
+    fn message(&mut self, id: u64, channel: u16, payload: &[u8]);
+}
