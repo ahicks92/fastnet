7fbde15bbb28d6236dc45e6f839712ffe9b04e75
Austin Hicks <camlorn@camlorn.net>
2016-04-26 22:23:05 -0400

    Simple client and server.

---
+++ examples/simple_client.rs
@@ -0,0 +1,24 @@
+extern crate fastnet;
+use std::net::{ToSocketAddrs};
+
+#[derive(Default)]
+struct EventHandler;
+
+impl fastnet::Handler for EventHandler {
+}
+
+fn main() {
+    let mut i = "localhost:1000".to_socket_addrs().unwrap();
+    let server_addr = i.next().unwrap();
+    i = "localhost:11000".to_socket_addrs().unwrap();
+    let our_addr = i.next().unwrap();
+    let maybe_serv = fastnet::Server::new(our_addr, EventHandler::default());
+    if let Err(ref what) = maybe_serv {
+        format!("Error: {:?}", what);
+    }
+    let mut serv = maybe_serv.unwrap();
+    serv.enable_debug_print();
+    serv.connect(server_addr, 0);
+    println!("Server is running.");
+    loop {}
+}
---
+++ examples/simple_server.rs
@@ -0,0 +1,21 @@
+extern crate fastnet;
+use std::net::{ToSocketAddrs};
+
+#[derive(Default)]
+struct EventHandler;
+
+impl fastnet::Handler for EventHandler {
+}
+
+fn main() {
+    let mut i = "localhost:10000".to_socket_addrs().unwrap();
+    let addr = i.next().unwrap();
+    let maybe_serv = fastnet::Server::new(addr, EventHandler::default());
+    if let Err(ref what) = maybe_serv {
+        format!("Error: {:?}", what);
+    }
+    let mut serv = maybe_serv.unwrap();
+    serv.enable_debug_print();
+    println!("Server is running.");
+    loop {}
+}
