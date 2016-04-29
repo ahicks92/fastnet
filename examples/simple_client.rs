b7448af6e62b99cbfb485bb96ce531dbe675e18e
Austin Hicks <camlorn@camlorn.net>
2016-04-28 22:52:24 -0400

    Make the examples use command line arguments.

--- examples/simple_client.rs
+++ examples/simple_client.rs
@@ -1,10 +1,16 @@
 extern crate fastnet;
 extern crate env_logger;
 use std::net::{ToSocketAddrs};
+use std::env;
 
 fn main() {
     env_logger::init().unwrap();
-    let mut i = "127.0.0.1:10000".to_socket_addrs().unwrap();
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() != 2{
+        println!("Syntax: {} <address>", args[0]);
+        return;
+    }
+    let mut i = args[1].to_socket_addrs().unwrap();
     let server_addr = i.next().unwrap();
     i = "0.0.0.0:11000".to_socket_addrs().unwrap();
     let our_addr = i.next().unwrap();
--- examples/simple_server.rs
+++ examples/simple_server.rs
@@ -1,10 +1,16 @@
 extern crate fastnet;
 extern crate env_logger;
 use std::net::{ToSocketAddrs};
+use std::env;
 
 fn main() {
     env_logger::init().unwrap();
-    let mut i = "0.0.0.0:10000".to_socket_addrs().unwrap();
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() != 2{
+        println!("Syntax: {} <address>", args[0]);
+        return;
+    }
+    let mut i = args[1].to_socket_addrs().unwrap();
     let addr = i.next().unwrap();
     let maybe_serv = fastnet::Server::new(addr, fastnet::PrintingHandler::new());
     if let Err(ref what) = maybe_serv {
