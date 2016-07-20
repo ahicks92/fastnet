7a6021cab759b89e10139ea578ddcd710768c1ce
Austin Hicks <camlorn@camlorn.net>
2016-07-20 16:30:30 -0400

    Structs for representing and building data packets with useful helper methods.

--- src/packets/mod.rs
+++ src/packets/mod.rs
@@ -55,3 +55,96 @@ pub const ABORTED_SPECIFIER: u8 = 4;
 pub const STATUS_FASTNET_SPECIFIER: u8 = 0;
 pub const STATUS_VERSION_SPECIFIER: u8 = 1;
 pub const STATUS_EXTENSION_SPECIFIER: u8 = 2;
+
+//Flag bits for data packets, used in the impl of the struct.
+pub const DATA_FRAME_START_BIT: u8 = 0;
+pub const DATA_FRAME_END_BIT: u8 = 1;
+pub const DATA_RELIABLE_BIT: u8 = 2;
+
+#[derive(Debug, Clone)]
+pub struct DataPacket {
+    sequence_number: u64,
+    flags: u8,
+    payload: Vec<u8>,
+}
+
+#[derive(Debug, Clone)]
+pub struct DataPacketBuilder {
+    sequence_number: u64,
+    is_reliable: bool,
+    is_start: bool,
+    is_end: bool,
+    payload: Vec<u8>,
+}
+
+impl DataPacketBuilder {
+    /**Initial state is unreliable, mid-frame, and empty payload.*/
+    pub fn new(sequence_number: u64)->DataPacketBuilder {
+        DataPacketBuilder::with_payload(sequence_number, Vec::default())
+    }
+
+    /**Configures the builder for mid-frame, unreliable but using the specified payload.*/
+    pub fn with_payload(sequence_number: u64, payload: Vec<u8>)->DataPacketBuilder {
+        DataPacketBuilder {
+            sequence_number: sequence_number,
+            is_reliable: false,
+            is_start: false,
+            is_end: false,
+            payload: payload,
+        }
+    }
+
+    pub fn set_payload(&mut self, payload: Vec<u8>) {
+        self.payload = payload;
+    }
+
+    pub fn set_reliable(&mut self, reliable: bool) {
+        self.is_reliable = reliable;
+    }
+
+    pub fn set_start(&mut self, start: bool) {
+        self.is_start = start;
+    }
+
+    pub fn set_end(&mut self, end: bool) {
+        self.is_end = end;
+    }
+
+    pub fn set_sequence_number(&mut self, sequence_number: u64) {
+        self.sequence_number = sequence_number;
+    }
+
+    pub fn build(self)->Option<DataPacket> {
+        let start_flag  = (self.is_start as u8) << DATA_FRAME_START_BIT;
+        let end_flag = (self.is_end as u8) << DATA_FRAME_END_BIT;
+        let reliable_flag = (self.is_reliable as u8) << DATA_RELIABLE_BIT;
+        let flags = start_flag | end_flag | reliable_flag;
+        Some(DataPacket {
+            sequence_number: self.sequence_number,
+            flags: flags,
+            payload: self.payload,
+        })
+    }
+}
+
+impl DataPacket {
+    pub fn is_reliable(&self)->bool {
+        (self.flags & DATA_RELIABLE_BIT) > 0
+    }
+
+    pub fn is_frame_start(&self)->bool {
+        (self.flags & DATA_FRAME_START_BIT) > 0
+    }
+
+    pub fn is_frame_end(&self)->bool {
+        (self.flags & DATA_FRAME_END_BIT) > 0
+    }
+
+    pub fn borrow_payload(&self)->&Vec<u8> {
+        &self.payload
+    }
+
+    pub fn into_payload(self)->Vec<u8> {
+        self.payload
+    }
+}
